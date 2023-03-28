337c0a23d6b9a142da8ffc74f125729776e463e7
Nivot73 <nivot73@gmail.com>
2023-03-28 11:33:50 +0200

    début de la construction de la page profil
    commencement du développement de la page profil avec affichage de l'image et pseudo suivant l'utilisateur.

--- pages/profil.php
+++ pages/profil.php
@@ -1,1 +1,20 @@
-page de profil de l'utilisateur
+<?php
+require_once('bdd/connect.php');
+
+    $id = strip_tags($_SESSION["id"]);
+
+    $sql = 'SELECT * FROM `utilisateurs` WHERE `id`=:id';
+    $query = $db->prepare($sql);
+    $query->bindValue(':id', $id);
+    $query->execute();
+    $info = $query->fetch(PDO::FETCH_ASSOC);
+
+require_once('bdd/close.php');
+?>
+
+
+<img class="imageProfil" src="images/imageJeu/<?= $info['image'] ?>" alt="">
+<p class="pseudoProfil" ><?= $info['nom'] ?></p>
+<div class="blocListAmis">
+
+</div>
