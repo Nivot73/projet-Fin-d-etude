bb6a7863d58e9a2402e312fdc209bda5056b0deb
Nivot73 <nivot73@gmail.com>
2023-03-30 10:49:04 +0200

    rajout des salon rejoins à la liste utilisateur

--- pages/listeUtilisateurs.php
+++ pages/listeUtilisateurs.php
@@ -21,8 +21,17 @@ foreach ($listUtilisateurs as $utilisateur) {
     <div class="boiteID">
         <img src="images/avatar/<?= $utilisateur['image'] ?>" alt="">
         <p><?= $utilisateur['nom'] ?></p>
-        <p><?= $utilisateur['information'] ?></p>
-        <p><?= $utilisateur['connexion'] ?></p>
+        <p>inscrit depuis le : <?= $utilisateur['information'] ?></p>
+        <?php
+        if($utilisateur['connexion'] == 0){
+            $connecter = "Deconnecter";
+        } 
+        elseif($utilisateur['connexion'] == 1) {
+            $connecter = "Connecter";
+        }
+        else {$connecter = "Etat inconnu";}
+        ?>
+        <p><?= $connecter ?></p>
     </div>
     <div class="boiteListJeu">
     <?php
@@ -51,7 +60,33 @@ foreach ($listUtilisateurs as $utilisateur) {
         </div>
     <?php } ?>
     </div>
-    <div class="boiteListSalon"></div>
+    <div class="boiteListSalon">
+    <?php
+
+    $id = $utilisateur['id'];
+
+    $sql = 'SELECT * FROM `salonrejoind` WHERE `id_utilisateurs`=:id LIMIT 4';
+    $query = $db->prepare($sql);
+    $query->bindValue(':id', $id);
+    $query->execute();
+    $listSalon = $query->fetchAll(PDO::FETCH_ASSOC);
+
+    foreach ($listSalon as $salon){
+
+        $id = $salon['id'];
+
+        $sql = 'SELECT * FROM `salons` WHERE `id`=:id';
+        $query = $db->prepare($sql);
+        $query->bindValue(':id', $id);
+        $query->execute();
+        $donnees = $query->fetch(PDO::FETCH_ASSOC);
+        ?>
+        <div class="boiteSalon">
+            <img src="images/imageJeu/<?= $donnees['image'] ?>" alt="">
+            <p><?= $donnees['nom']; ?></p>
+        </div>
+    <?php } ?>
+    </div>
 </div>
 
 <?php } 
