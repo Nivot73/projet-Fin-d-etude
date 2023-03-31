e2f9c46fe421bcf9621fbb5532927dfeae609370
Nivot73 <nivot73@gmail.com>
2023-03-31 15:24:15 +0200

    CSS page jeu simple

--- pages/jeu.php
+++ pages/jeu.php
@@ -21,6 +21,18 @@ if(isset($_GET['id']) && !empty($_GET['id']))
     $query->execute();
     $genreJeu = $query->fetchAll(PDO::FETCH_ASSOC);
 
+    $sql = 'SELECT * FROM `salons` WHERE `id_jeux`=:id';
+    $query = $db->prepare($sql);
+    $query->bindValue(':id', $id);
+    $query->execute();
+    $salons = $query->fetchAll(PDO::FETCH_ASSOC);
+
+    $sql = 'SELECT * FROM `jeujouer` WHERE `id`=:id';
+    $query = $db->prepare($sql);
+    $query->bindValue(':id', $id);
+    $query->execute();
+    $joueurs = $query->fetchAll(PDO::FETCH_ASSOC);
+
 }
 else{
     header("location:index.php");
@@ -54,5 +66,69 @@ else{
     </div>
     <p>Description : <?= $jeu['description'] ?></p>
 </div>
-<div class="listDansJeu"></div>
+<div class="listDansJeu">
+    <div>
+        <p>Joueurs</p>
+        <table>
+            <thead>
+                <tr>
+                    <td>Image</td>
+                    <td>Nom</td>
+                    <td>Etat</td>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                foreach($joueurs as $joueur){
+
+                    $id = $joueur['id'];
+
+                    $sql = 'SELECT * FROM `utilisateurs` WHERE `id`=:id';
+                    $query = $db->prepare($sql);
+                    $query->bindValue(':id', $id);
+                    $query->execute();
+                    $info = $query->fetch(PDO::FETCH_ASSOC);
+                ?>
+                    <tr>
+                        <td><img  class="imageListDansJeu" src="images/imageSalon/<?= $info['image'] ?>" alt=""></td>
+                        <td><?= $info['nom'] ?></td>
+                        <td>
+                           <?php 
+                            if($info['connexion'] == 1)
+                            { echo "Connecté";}
+                            else
+                            { echo"Deconnecter";}
+                            ?>
+                        </td>
+                    </tr>
+                <?php } ?>
+
+
+            </tbody>
+        </table>
+    </div>
+    <div>
+        <p>Salons</p>
+        <table>
+            <thead>
+                <tr>
+                    <td>Image</td>
+                    <td>Nom</td>
+                    <td>Description</td>
+                </tr>
+            </thead>
+            <tbody>
+            <?php
+            foreach($salons as $salon){
+            ?>
+                <tr>
+                    <td><img  class="imageListDansJeu" src="images/imageSalon/<?= $salon['image'] ?>" alt=""></td>
+                    <td><?= $salon['nom'] ?></td>
+                    <td><?= $salon['description'] ?></td>
+                </tr>
+            <?php } ?>
+            </tbody>
+        </table>
+    </div>
+</div>
 <?php require_once('bdd/close.php'); ?>
