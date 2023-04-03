666ab14c999b9a1fd325121178956a76e7894922
Nivot73 <nivot73@gmail.com>
2023-04-03 15:19:32 +0200

    ajout des lien entre éléments dans la page profil

--- pages/profil.php
+++ pages/profil.php
@@ -47,7 +47,7 @@ require_once('bdd/connect.php');
             $donnees = $query->fetch(PDO::FETCH_ASSOC);
         ?>
 
-        <div class="blockAmis">
+        <a href="?page=utilisateur&id=<?= $donnees['id'] ?>" class="blockAmis">
             <img src="images/avatar/<?= $donnees['image'] ?>" alt="">
             <div>
                 <p class="blockAmisNom"><?= $donnees['nom'] ?></p>
@@ -61,7 +61,7 @@ require_once('bdd/connect.php');
                     <?php } 
                     echo $donnees['information']; ?></p>
             </div>
-        </div>
+        </a>
         <?php } ?>
     </div>
     <div class="blockList">
@@ -79,40 +79,40 @@ require_once('bdd/connect.php');
             $donnees = $query->fetch(PDO::FETCH_ASSOC);
         ?>
 
-        <div class="blockAmis">
+        <a href="?page=jeu&id=<?= $donnees['id'] ?>" class="blockAmis">
             <img src="images/avatar/<?= $donnees['image'] ?>" alt="">
             <div>
                 <p class="blockAmisNom"><?= $donnees['nom'] ?></p>
 
                 <p>sortie le : <?= $donnees['date_de_sortie']; ?></p>
             </div>
-        </div>
+        </a>
         <?php } ?>
     </div>
     <div class="blockList">
         <p>Salons</p>
 
-    <?php
-        foreach ($listSalons as $salon) {
+        <?php
+            foreach ($listSalons as $salon) {
 
-        $id = $salon['id'];
+            $id = $salon['id'];
 
-        $sql = 'SELECT * FROM `salons` WHERE `id`=:id';
-        $query = $db->prepare($sql);
-        $query->bindValue(':id', $id);
-        $query->execute();
-        $donnees = $query->fetch(PDO::FETCH_ASSOC);
-    ?>
+            $sql = 'SELECT * FROM `salons` WHERE `id`=:id';
+            $query = $db->prepare($sql);
+            $query->bindValue(':id', $id);
+            $query->execute();
+            $donnees = $query->fetch(PDO::FETCH_ASSOC);
+        ?>
 
-    <div class="blockAmis">
-        <img src="images/avatar/<?= $donnees['image'] ?>" alt="">
-        <div>
-            <p class="blockAmisNom"><?= $donnees['nom'] ?></p>
+        <a href="?page=salon&id=<?= $donnees['id'] ?>" class="blockAmis">
+            <img src="images/avatar/<?= $donnees['image'] ?>" alt="">
+            <div>
+                <p class="blockAmisNom"><?= $donnees['nom'] ?></p>
 
-            <p><?= $donnees['description']; ?></p>
-        </div>
-    </div>
-    <?php } ?>
+                <p><?= $donnees['description']; ?></p>
+            </div>
+        </a>
+        <?php } ?>
     </div>
 </div>
 
