b8ab4ae73a353fe5c4b0ecbe2f8e4140442e40be
Nivot73 <nivot73@gmail.com>
2023-04-04 15:04:24 +0200

    début de développement page salons simple

--- pages/salon.php
+++ pages/salon.php
@@ -1,1 +1,23 @@
-page de salon <?= $_GET['id']; ?>
+<?php
+if(isset($_GET['id']) && !empty($_GET['id']))
+{
+    require_once('bdd/connect.php');
+
+    $id = strip_tags($_GET["id"]);
+
+    $sql = 'SELECT * FROM `salons` WHERE `id`=:id';
+    $query = $db->prepare($sql);
+    $query->bindValue(':id', $id);
+    $query->execute();
+    $info = $query->fetch(PDO::FETCH_ASSOC);
+
+    $sql = 'SELECT * FROM `salons` WHERE `id_salons`=:id';
+    $query = $db->prepare($sql);
+    $query->bindValue(':id', $id);
+    $query->execute();
+    $commentaire = $query->fetchAll(PDO::FETCH_ASSOC);
+
+    require_once('bdd/close.php');
+}
+else{header("location:index.php");}
+?>
